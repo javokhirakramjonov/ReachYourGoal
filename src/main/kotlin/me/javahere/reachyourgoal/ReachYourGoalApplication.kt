c49e6fbea00d142578b3e0f1c2ef958497ecaa37
Javokhir <javokhir.akramjonov@gmail.com>
2023-12-04 11:13:05 +0100

    initial commit

---
+++ src/main/kotlin/me/javahere/reachyourgoal/ReachYourGoalApplication.kt
@@ -0,0 +1,11 @@
+package me.javahere.reachyourgoal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReachYourGoalApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReachYourGoalApplication>(*args)
+}
---
+++ src/test/kotlin/me/javahere/reachyourgoal/ReachYourGoalApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.javahere.reachyourgoal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReachYourGoalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
