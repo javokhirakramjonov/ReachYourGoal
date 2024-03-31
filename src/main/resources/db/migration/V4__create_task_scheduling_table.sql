738839e305bbe5599f196de1230c2404da6693d1
Javokhir <javokhir.akramjonov@gmail.com>
2024-04-01 01:44:32 +0200

    refactored flyway migrations

--- src/main/resources/db/migration/V4__create_task_scheduling_table.sql
+++ src/main/resources/db/migration/V4__create_task_scheduling_table.sql
@@ -3,5 +3,5 @@ CREATE TABLE task_scheduling
     id             BIGSERIAL PRIMARY KEY,
     task_id        UUID REFERENCES tasks (id) ON DELETE CASCADE,
     task_date_time TIMESTAMP    NOT NULL,
-    task_status    VARCHAR(255) NOT NULL
+    task_status    VARCHAR(50) NOT NULL
 );
