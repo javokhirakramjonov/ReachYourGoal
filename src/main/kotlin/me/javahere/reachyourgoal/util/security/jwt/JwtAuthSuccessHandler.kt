eede83a3ce23f5e2d5ddd4d686fdcf31e1204e2f
Javokhir <javokhir.akramjonov@gmail.com>
2024-04-18 00:48:10 +0200

    fix some minor bug

--- src/main/kotlin/me/javahere/reachyourgoal/util/security/jwt/JwtAuthSuccessHandler.kt
+++ src/main/kotlin/me/javahere/reachyourgoal/util/security/jwt/JwtAuthSuccessHandler.kt
@@ -39,7 +39,7 @@ class JwtAuthSuccessHandler(
                             .map(GrantedAuthority::getAuthority)
                             .toTypedArray()
 
-                    val userId = userService.findUserByUsername(principal.username).id.toString()
+                    val userId = userService.findUserByUsername(principal.username).id.value.toString()
 
                     val accessToken =
                         jwtService.generateAccessToken(userId, principal.username, EXPIRE_ACCESS_TOKEN, roles)
